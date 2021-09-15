2ff480254d067b7dcc9a7014ed460d2225a185f5
Carolina de Senne Garcia <carolina.senne@vtex.com.br>
2021-09-15 20:43:44 +0200

    Create servers for testing

--- ConsistentHashBoundedLoads/Program.cs
+++ ConsistentHashBoundedLoads/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace ConsistentHashBoundedLoads
+﻿using System.Collections.Generic;
+
+namespace ConsistentHashBoundedLoads
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+        }
+
+        public static void RunConsistentHash()
+        {
+            List<Server> servers = new List<Server>();
+            for (int i = 10; i < 25; i++)
+                servers.Add(new Server($"{i}", 100));
+
+            ConsistentHashBoundedLoads CHBL = new ConsistentHashBoundedLoads(servers.ToArray());
+            
+            // look for examples:
+            // index=janus DetailedLogs api_cache_status!="NOAPICACHE"
+            // host pathAndQuery
+            
         }
     }
 }
