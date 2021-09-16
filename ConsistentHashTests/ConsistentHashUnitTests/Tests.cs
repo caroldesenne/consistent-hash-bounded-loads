fcf30b4123d7e505a4eebfd503b189b41279375a
Carolina de Senne Garcia <carolina.senne@vtex.com.br>
2021-09-16 19:08:30 +0200

    Add test to show that we do not necessarily choose the least loaded server

--- ConsistentHashTests/ConsistentHashUnitTests/Tests.cs
+++ ConsistentHashTests/ConsistentHashUnitTests/Tests.cs
@@ -17,9 +17,28 @@ namespace ConsistentHashUnitTests
             };
             var ch = new ConsistentHash(servers);
             ch.InflightRequests = 425;
+            ch.Load = 1.1;
             Assert.Equal("4", ch.Next("blalba", "bloblo").id);
             Assert.Equal(100, ch.NextBoundedTryNext("blalba", "bloblo").inflightRequest);
             Assert.Equal(100, ch.NextBoundedTryHash("blalba", "bloblo").inflightRequest);
+        }
+        
+        [Fact]
+        public void TestDoesNotGetLighterServer()
+        {
+            var servers = new Server[]
+            {
+                new ("1", 100),
+                new ("2", 125),
+                new ("3", 125),
+                new ("4", 125)
+            };
+            var ch = new ConsistentHash(servers);
+            ch.InflightRequests = 475;
+            ch.Load = 1.1;
+            Assert.Equal("4", ch.Next("blalba", "bloblo").id);
+            Assert.NotEqual(100, ch.NextBoundedTryNext("blalba", "bloblo").inflightRequest);
+            Assert.NotEqual(100, ch.NextBoundedTryHash("blalba", "bloblo").inflightRequest);
         }
     }
 }
